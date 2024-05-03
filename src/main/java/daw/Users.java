1fdccc146bbf49f687e6e86867c69a0a62341abc
kracelh542 <kracelh542@gmail.com>
2024-05-03 10:16:23 +0200

    terminar boton login y registro

---
+++ src/main/java/daw/Users.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author khalid
+ */
+class Users {
+    
+    private String username;
+    private String password;
+
+    public Users(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+}
